8d7a91f227ca7e3b3fdf53637e0bbff14a000a35
nbrader <nbrader+github@gmail.com>
2024-05-09 23:54:59 +0100

    Removed probably unneeded lemma attempts.

--- Coq/BirdMeertens/Lemmas.v
+++ Coq/BirdMeertens/Lemmas.v
@@ -33,68 +33,6 @@ Proof.
   apply concat_map.
 Qed.
 
-Lemma foldl_promotion : RfoldlSum ∘ concat = RfoldlSum ∘ map RfoldlSum.
-Proof.
-  apply functional_extensionality.
-  intros.
-  induction x as [|x xs IH]; simpl.
-  - reflexivity. (* Base case: both sides are empty *)
-  - unfold compose.
-    rewrite concat_cons.
-    rewrite map_cons.
-    unfold RfoldlSum.
-    simpl.
-    rewrite Rplus_0_l.
-    rewrite foldl_left_app.
-    fold RfoldlSum.
-    induction xs.
-    + simpl.
-      reflexivity.
-    + simpl.
-      rewrite foldl_left_app.
-      admit.
-Admitted.
-
-Lemma foldl_promotion1 : RfoldlSum ∘ concat = RfoldlSum ∘ map RfoldlSum.
-Proof.
-  apply functional_extensionality.
-  intros.
-  induction x as [|x xs IH]; simpl.
-  - reflexivity. (* Base case: both sides are empty *)
-  - unfold compose.
-    induction x, xs.
-    + unfold concat, RfoldlSum.
-      simpl.
-      f_equal.
-      ring.
-    + rewrite concat_cons.
-      rewrite app_nil_l.
-      unfold compose in IH.
-      unfold concat in IH.
-      rewrite IH.
-      rewrite map_cons.
-      rewrite map_cons.
-      rewrite map_cons.
-      unfold RfoldlSum.
-      simpl.
-      rewrite Rplus_0_l.
-      rewrite Rplus_0_l.
-      rewrite Rplus_0_l.
-      fold RfoldlSum.
-      reflexivity.
-    + rewrite map_cons.
-      simpl.
-      rewrite app_nil_r.
-      unfold RfoldlSum.
-      simpl.
-      rewrite Rplus_0_l.
-      rewrite Rplus_0_l.
-      reflexivity.
-    + admit.
-Admitted.
-
-(* Lemma maximum_distr (xs ys : list (option R)) : maximum (xs ++ ys) = (maximum xs) <|> (maximum ys). *)
-
 Lemma fold_promotion : maximum ∘ concat = maximum ∘ map maximum.
 Proof.
   unfold compose.
